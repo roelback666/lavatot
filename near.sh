73dec16a36fc083dfda20da41d24b5c16342c52b
roelback666 <108212351+roelback666@users.noreply.github.com>
2024-05-02 22:14:22 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-9874920e-67c1-4f72-8157-8a8f9d03d44c/"
 
 OUTPUT_DIR="logs"
 
